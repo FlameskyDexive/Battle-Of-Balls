498469888621308b64880b198c179bfa22a3fa79
Flamesky <flamesky@live.com>
2018-07-11 23:18:51 +0800

    添加球球目录，升级工程到unity2018.2，hotfix默认用.net4.7.1
    添加球球目录，升级工程到unity2018.2，hotfix默认用.net4.7.1

---
+++ Unity/Hotfix/BattleOfBalls/UI/UILoginBalls/Event/InitSceneStart_CreateLoginBallsUI.cs
@@ -0,0 +1,13 @@
+﻿using ETModel;
+
+namespace ETHotfix
+{
+	[Event(EventIdType.InitSceneStart)]
+	public class InitSceneStart_CreateLoginBallsUI : AEvent
+	{
+		public override void Run()
+		{
+			UI ui = Game.Scene.GetComponent<UIComponent>().Create(UIType.UILoginBalls);
+		}
+	}
+}
